22a737d7ef48e75cd6fe26c7002c5024444a2cd1
Paul Albee <Paul@Albee.com>
2011-07-05 11:38:42 -0400

    Added stack trace code

--- webclient/pcp.js
+++ webclient/pcp.js
@@ -105,6 +105,7 @@ function populate_workflows(newContent){
     });
     
     if(errors.length != 0){
+        printStackTrace();
         report_errors(new Error(errors));
         return;
     }
@@ -171,3 +172,54 @@ function uploadFile(evt) {
 }
 
 
+function printStackTrace() {
+  var callstack = [];
+  var isCallstackPopulated = false;
+  try {
+    i.dont.exist+=0; //doesn't exist- that's the point
+  } catch(e) {
+    if (e.stack) { //Firefox
+      var lines = e.stack.split('\n');
+      for (var i=0, len=lines.length; i&lt;len; i++) {
+        if (lines[i].match(/^\s*[A-Za-z0-9\-_\$]+\(/)) {
+          callstack.push(lines[i]);
+        }
+      }
+      //Remove call to printStackTrace()
+      callstack.shift();
+      isCallstackPopulated = true;
+    }
+    else if (window.opera &amp;&amp; e.message) { //Opera
+      var lines = e.message.split('\n');
+      for (var i=0, len=lines.length; i&lt;len; i++) {
+        if (lines[i].match(/^\s*[A-Za-z0-9\-_\$]+\(/)) {
+          var entry = lines[i];
+          //Append next line also since it has the file info
+          if (lines[i+1]) {
+            entry += ' at ' + lines[i+1];
+            i++;
+          }
+          callstack.push(entry);
+        }
+      }
+      //Remove call to printStackTrace()
+      callstack.shift();
+      isCallstackPopulated = true;
+    }
+  }
+  if (!isCallstackPopulated) { //IE and Safari
+    var currentFunction = arguments.callee.caller;
+    while (currentFunction) {
+      var fn = currentFunction.toString();
+      var fname = fn.substring(fn.indexOf(&amp;quot;function&amp;quot;) + 8, fn.indexOf('')) || 'anonymous';
+      callstack.push(fname);
+      currentFunction = currentFunction.caller;
+    }
+  }
+  output(callstack);
+}
+ 
+function output(arr) {
+  //Optput however you want
+  alert(arr.join('\n\n'));
+}
