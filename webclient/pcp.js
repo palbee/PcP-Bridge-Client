847a9b67cd149202b314f309597d6ff7ee5bbb94
Paul Albee <Paul@Albee.com>
2011-07-05 12:37:07 -0400

    Updated web client to allow for Return to login.

--- webclient/pcp.js
+++ webclient/pcp.js
@@ -66,6 +66,8 @@ dojo.ready(function(){
     dojo.connect(dojo.byId("title"), "onkeyup", uploadEnable);
     dojo.connect(dojo.byId("description"), "onkeyup", uploadEnable);
     dojo.connect(dojo.byId("file"), "onchange", uploadEnable);
+    dojo.connect(dojo.byId("user"), "onkeypress", function(evt){if(evt.keyCode == 13) dojo.byId("password").focus();});
+    dojo.connect(dojo.byId("password"), "onkeypress", function(evt){if(evt.keyCode == 13) retrieveWorkflows(evt);});
     dojo.connect(dojo.byId("workflow"), "onchange", function(evt){dojo.byId("uuid").value = dojo.byId("workflow").value;});
     uploadEnable();     
 });
@@ -169,57 +171,4 @@ function uploadFile(evt) {
           sync: true,
       });
     
-}
-
-
-function printStackTrace() {
-  var callstack = [];
-  var isCallstackPopulated = false;
-  try {
-    i.dont.exist+=0; //doesn't exist- that's the point
-  } catch(e) {
-    if (e.stack) { //Firefox
-      var lines = e.stack.split('\n');
-      for (var i=0, len=lines.length; i&lt;len; i++) {
-        if (lines[i].match(/^\s*[A-Za-z0-9\-_\$]+\(/)) {
-          callstack.push(lines[i]);
-        }
-      }
-      //Remove call to printStackTrace()
-      callstack.shift();
-      isCallstackPopulated = true;
-    }
-    else if (window.opera &amp;&amp; e.message) { //Opera
-      var lines = e.message.split('\n');
-      for (var i=0, len=lines.length; i&lt;len; i++) {
-        if (lines[i].match(/^\s*[A-Za-z0-9\-_\$]+\(/)) {
-          var entry = lines[i];
-          //Append next line also since it has the file info
-          if (lines[i+1]) {
-            entry += ' at ' + lines[i+1];
-            i++;
-          }
-          callstack.push(entry);
-        }
-      }
-      //Remove call to printStackTrace()
-      callstack.shift();
-      isCallstackPopulated = true;
-    }
-  }
-  if (!isCallstackPopulated) { //IE and Safari
-    var currentFunction = arguments.callee.caller;
-    while (currentFunction) {
-      var fn = currentFunction.toString();
-      var fname = fn.substring(fn.indexOf(&amp;quot;function&amp;quot;) + 8, fn.indexOf('')) || 'anonymous';
-      callstack.push(fname);
-      currentFunction = currentFunction.caller;
-    }
-  }
-  output(callstack);
-}
- 
-function output(arr) {
-  //Optput however you want
-  alert(arr.join('\n\n'));
 }
